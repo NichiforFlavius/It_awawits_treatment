dd7a38af9274c613232d0c733e8cf02a5c83db42
Flavius Nichifor <flavius_nichifor@yahoo.com>
2024-09-26 20:09:19 +0300

    Added platforms and dying/respawning
    Changed the layout of the demo level.
    Added moving platforms for the player to collide with and traverse gaps that can't be jumped.
    Added dying/respawning mechanic if the player reaches an out-of-bounds killzone area.

--- it_awaits_treatment/scripts/player.gd
+++ it_awaits_treatment/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -400.0
 
 enum State {IDLE, WALKING, JUMPING, ATTACKING, DYING}
 var current_state = State.IDLE
